ac452a9b62fd876077af98e412c6613ab2d66e1d
donghae <donghae1999@gmail.com>
2023-05-24 14:40:13 +0900

    feat: CORS 설정
    와일드 카드 *

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -4,6 +4,7 @@ import cart.dao.MemberDao;
 import cart.ui.MemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,13 +12,18 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private final MemberDao memberDao;
-
     public WebMvcConfig(MemberDao memberDao) {
         this.memberDao = memberDao;
     }
-
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new MemberArgumentResolver(memberDao));
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        // TODO Basic 인증 검증 필요
+        corsRegistry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOriginPatterns("*");
     }
 }
